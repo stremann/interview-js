846496c462089a371b47628105f45462ac74eecd
Roman Stremedlovskyi <stremann@users.noreply.github.com>
2017-10-24 18:39:22 +0300

    feat(008) add flatten array

--- 008-flatten-array.js
+++ 008-flatten-array.js
@@ -1,5 +1,18 @@
-function flatten(data) {
-    return data.reduce((acc, curr) => acc.concat(Array.isArray(curr) ? flatten(curr) : curr), []);
+function flattenRecursive(data) {
+    return data.reduce((acc, curr) => acc.concat(Array.isArray(curr) ? flattenRecursive(curr) : curr), []);
 }
 
-flatten([1, 2, [3, 4, [5, 6, [7, 8, 9, [10, 11], 12], 13], [14, 15], 16], 17]); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+function flattenNotRecursive(data) {
+    let clone = data.slice();
+    const flat = [];
+    while (clone.length) {
+        const el = clone.shift();
+        Array.isArray(el) ? clone = el.concat(clone) : flat.push(el);
+    }
+    return flat;
+}
+
+const data = [1, 2, [3, 4, [5, 6, [7, 8, 9, [10, 11], 12], 13], [14, 15], 16], 17];
+
+flattenRecursive(data); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+flattenNotRecursive(data); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
